718a78af7486492dcbdb2eadf1cd989ae700f21f
kiran <kiranfuse52@gmail.com>
2023-05-03 11:24:46 +0530

    Updated code : 03/05/2023

---
+++ UserApp/src/main/java/com/prashant/backgroundcallername/Util/LocalData.java
@@ -0,0 +1,36 @@
+package com.prashant.backgroundcallername.Util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.prashant.backgroundcallername.Ui.MainActivity;
+
+public class LocalData {
+    static SharedPreferences preferences;
+    static SharedPreferences.Editor editor;
+    public static void saveDeviceID (Context context, String deviceId){
+
+        preferences = context.getSharedPreferences("users", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        editor.putString("deviceId", deviceId);
+        editor.commit();
+
+    }
+
+    public static String  getDeviceID (Context context){
+        preferences = context.getSharedPreferences("users", Context.MODE_PRIVATE);
+        String deviceID = preferences.getString("deviceId", "");
+        return deviceID;
+    }
+
+    public static void  check (Context context){
+        preferences = context.getSharedPreferences("users", Context.MODE_PRIVATE);
+
+        if (preferences.contains("deviceId")){
+            context.startActivity(new Intent(context, MainActivity.class));
+        }
+
+    }
+
+}
